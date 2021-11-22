83b24cea76e1daaea518132ffe297410abd7f00f
Alexander Harris <alexharris@alexanders-mbp.attlocal.net>
2021-11-22 16:44:30 -0500

    adds Chef model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class Chef(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    bio = models.TextField(max_length=500)
+    location = models.CharField(max_length=100)
+    shift = models.CharField(max_length=20)
+    hourly = models.IntegerField()
+    phone = models.CharField(max_length=12)
+    booked_dates = models.DateTimeField('Booked Dates')
+
+
+
+    
