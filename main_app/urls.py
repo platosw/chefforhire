f333a7b24ece61e421ea749bbb9da005f34b3410
Tyrone-Cartwright <tyrone.cartwright@yahoo.com>
2021-11-23 15:22:39 -0500

    added Detail

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
   path('', views.landing, name='landing'),
   path('about/', views.about, name='about'),
   path('chefs/', views.ChefsIndex.as_view(), name='chefs'),
+  path('chefs/<int:pk>/', views.ChefsDetail.as_view(), name='chefs_detail'),
 ]
