91121a50b753595c29882f4654fe996bfcaa0177
Tyrone-Cartwright <tyrone.cartwright@yahoo.com>
2021-11-23 13:54:36 -0500

    added Chef class index in views.py and added path to chefs in urls.py

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Chef
 
 # Create your views here.
 def landing(request):
   return render(request, 'landing.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+# Chefs class
+class ChefsIndex(ListView):
+  model = Chef
+  template_name = 'chefs/index.html'
